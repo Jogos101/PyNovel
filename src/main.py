18c72780c5365a951994764d971dd2a1e662e088
Simon Lucena <simonlucena8@gmail.com>
2026-03-11 16:47:08 -0300

    Padronização de projeto
    - Padronização da classe main
    - Criação da classe de coleta de dados

--- src/main.py
+++ src/main.py
@@ -1,52 +1,5 @@
 from controller.PyNovelController import PyNovelController as PyNovel
-from entity.Livro import Livro
-from entity.Fonte import Fonte
-import inquirer
+from controller.ColetarDadosController import ColetarDadosController as ColetarDados
 
-# Coletar o método de coleta de dados
-questions = [
-  inquirer.List('WebScraper',
-    message="Qual método de coleta você deseja?",
-    choices=['Request(recommended)', 'Selenium'],
-  ),
-]
-answers = inquirer.prompt(questions)
-
-
-metodo = answers["WebScraper"]
-
-# Dados do livro
-titulo_livro = "Immortality Through Array Formations"
-autor = "Observing the Emptiness, 观虚"
-idioma = "en"
-
-# Url sem o padrão de capítulos
-url_inicial = "https://freewebnovel.com/novel/immortality-through-array-formations/chapter-1"
-# Quantidade de capítulos
-total_capitulos = 10
-# Dados HTML
-class_titulo = None
-tag_titulo = "h4"
-class_conteudo = "txt"
-id_conteudo = None
-url_padrao = True
-next_chap = "next_chap"
-next_disabled = "disabled"
-tag_conteudo = "p"
-
-livro = Livro(titulo_livro, autor, idioma)
-# url, url_padrao, total_capitulos, class_titulo, tag_titulo, class_conteudo, next_chap, next_end, tag_conteudo
-fonte = Fonte(url_inicial, url_padrao, total_capitulos, class_titulo, tag_titulo, class_conteudo, next_chap, next_disabled, tag_conteudo)
-
-PyNovel(fonte, livro, metodo).start()
-
-# titulo = {
-#     "id": None,
-#     "class": None,
-#     "tag": "h4"
-# }
-# conteudo = {
-#     "id": None,
-#     "class": "txt",
-#     "tag": None
-# }
+if __name__ == "__main__":
+  PyNovel(*ColetarDados().coletar()).start()
