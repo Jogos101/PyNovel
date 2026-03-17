03a8efd4089b4b1d9397d592e7187e8bd1d59421
Simon Lucena <simonlucena8@gmail.com>
2026-03-17 15:55:45 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@ from application.PyNovelApplication import PyNovelApplication
 from controller.PyNovelController import PyNovelController as PyNovel
 from controller.ColetarDadosController import ColetarDadosController as ColetarDados
 from factory.WebScraperFactory import WebScraperFactory
-from services import EpubService
+from services import epub_service
 
 def main():
   app = PyNovelApplication(
     coletar_dados_controller= ColetarDados(),
     webscraper_factory= WebScraperFactory,
-    epub_service= EpubService,
+    epub_service= epub_service,
     pynovel_controller= PyNovel
   )
   app.run()
