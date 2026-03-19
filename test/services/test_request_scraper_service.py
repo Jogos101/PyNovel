0d34c86895c6b4725d2f48c6641ffb2d48146adb
Simon Lucena <simonlucena8@gmail.com>
2026-03-19 17:25:40 -0300

    Update test_request_scraper_service.py

--- test/services/test_request_scraper_service.py
+++ test/services/test_request_scraper_service.py
@@ -7,13 +7,27 @@ from src.services.request_scraper_service import RequestScraperService
 
 class TestRequestScraperService(unittest.TestCase):
     def setUp(self):
-        self.request_scraper_service = RequestScraperService(
-            Fonte(
-                url_inicial="https://www.novel.com/book/titulo/Chapter-1",
-                getTitulo={"id": "chapter-title"},
-                getConteudo={"class": "chapter-content"},
+        self.fonte = Fonte(
+                url="https://site/chapter-1",
+                url_padrao=True,
+                total_capitulos=15,
+                titulo={
+                    "id": None,
+                    "class": "char-titulo",
+                    "tag": None,
+                },
+                conteudo={
+                    "id": "char-conteudo",
+                    "class": None,
+                    "tag": None,
+                },
+                next_chap=None,
+                next_disabled="disabled",
+                tag_conteudo="p",
             )
-        )
+        self.request_scraper_service = RequestScraperService(self.fonte)
+
+        self.request_patcher = unittest.mock.patch("requests.get")
 
 if __name__ == "__main__":
     unittest.main()
