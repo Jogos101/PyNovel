208de24be5764115e7148796aa3f42d397ac00d9
Simon Lucena <simonlucena8@gmail.com>
2026-03-23 16:30:02 -0300

    Implementação de nova biblioteca
    Implementação do EbookLib

--- src/services/ebook_lib_service.py
+++ src/services/ebook_lib_service.py
@@ -1,11 +1,11 @@
 import os
-
 import ebooklib # type: ignore
 from ebooklib import epub # type: ignore
 from src.services.epub_service import EpubService
 import src.entity.livro
 import src.entity.capitulo 
 from pathlib import Path
+import uuid
 
 class EbookLibService(EpubService):
     def __init__(self, livro):
@@ -20,32 +20,83 @@ class EbookLibService(EpubService):
         if self.ebook is None:
             self.ebook = epub.EpubBook()
 
+            if self.livro.cover is not None:
+                self.set_cover()
+            self.ebook.set_title(self.livro.titulo)
+            self.ebook.set_language(self.livro.idioma)
+            self.ebook.add_author(self.livro.autor)
+            self.ebook.set_identifier(str(uuid.uuid4()))
+
+    def set_style(self, BASE_PATH=Path(__file__).resolve().parent.parent.parent):
+        c = epub.EpubItem()
+        c.file_name = 'style/style.css'
+        c.media_type = 'text/css'
+
+        style_path = BASE_PATH / 'src' / 'layout' / 'style' / 'style.css'
+        if not style_path.exists():
+            raise FileNotFoundError(f"Arquivo de estilo não encontrado: {style_path}") 
+        
+        with open(style_path, 'r') as style_file:
+            c.content = style_file.read()
+        self.ebook.add_item(c)
+
+    def set_cover(self):
+        try:
+            with open(self.livro.cover, 'rb') as cover_file:
+                cover_data = cover_file.read()
+                self.ebook.set_cover(os.path.basename(self.livro.cover), cover_data)
+        except Exception as e:
+            print(f"Erro ao definir a capa: {e}")
+
     def getSetEbook(self, file):
         if self.ebook is None:
             self.ebook = epub.read_epub(file)
 
     def setToc(self):
-        self.ebook.toc = (
-            (epub.Section(self.livro.get_titulo_limpo()), self.lista_capitulos),
-        )
+        self.ebook.toc = (self.lista_capitulos)
+        self.ebook.spine = ['nav'] + self.lista_capitulos
 
     def criar_capitulo(self, capitulo):
         title=capitulo.titulo
         file_name=f'{capitulo.get_file_name()}.xhtml'
-        content=capitulo.conteudo
         lang= self.livro.idioma
 
         chapter = epub.EpubHtml(
             title=title,
             file_name=file_name,
-            content=content,
-            lang=lang
+            lang=lang,
             )
+        
+        chapter.set_content(self.formatar_conteudo(capitulo))
+        
         self.ebook.add_item(chapter)
         self.lista_capitulos.append(chapter)
 
+    def formatar_conteudo(self, capitulo):
+        content = capitulo.conteudo.strip() if isinstance(capitulo.conteudo, str) else str(capitulo.conteudo)
+        # Formatar o conteúdo para XHTML
+        html_content = f'''
+            <html xmlns="http://www.w3.org/1999/xhtml">
+            <head>
+                <meta charset="UTF-8" />
+                <link rel="stylesheet" type="text/css" href="style/style.css" />
+            </head>
+            <body>
+                <h1>{capitulo.titulo}</h1>
+                {content}
+            </body>
+            </html>
+        '''
+        return html_content.encode('utf-8')
+
     def gerar_epub(self):
+        self.set_style()
+
         self.setToc()
+
+        self.ebook.add_item(epub.EpubNcx())
+        self.ebook.add_item(epub.EpubNav())
+
         arquivo = self.set_arquivo()
         epub.write_epub(str(arquivo), self.ebook)
 
